90f717b4603a6bb0d22de32bebcee865c07e71c1
deqing.qdq <deqing.qdq@alibaba-inc.com>
2017-03-15 18:20:14 +0800

    finish

---
+++ src/app/view/common/footer.js
@@ -0,0 +1,11 @@
+define('app/view/common/footer',['require','module','exports',"magix"],function(require,module,exports){
+ var Magix = require('magix')
+
+ module.exports = Magix.View.extend({
+     tmpl: "<header><h2>footer</h2></header>",
+     render: function() {
+         this.setHTML(this.id, this.tmpl)
+     }
+ })
+
+});
---
+++ src/app/view/common/header.js
@@ -0,0 +1,11 @@
+define('app/view/common/header',['require','module','exports',"magix"],function(require,module,exports){
+ var Magix = require('magix')
+
+ module.exports = Magix.View.extend({
+     tmpl: "<header><h2>header</h2></header>",
+     render: function() {
+         this.setHTML(this.id, this.tmpl)
+     }
+ })
+
+});
