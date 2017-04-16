08e319a065e38424c3ec4e09d7f3d7d92b33f827
deqing.qdq <deqing.qdq@alibaba-inc.com>
2017-04-16 15:09:32 +0800

    finish step06

--- src/app/view/todo/add.js
+++ src/app/view/todo/add.js
@@ -1,10 +1,37 @@
-define('app/view/todo/add',['require','module','exports',"magix"],function(require,module,exports){
+define('app/view/todo/add',['require','module','exports',"magix","jquery"],function(require,module,exports){
 var Magix = require('magix')
+var $ = require('jquery')
 
 module.exports = Magix.View.extend({
-    tmpl: "<div>todo add</div>",
+    tmpl: "<div><h2>新建Todo</h2><form mx-submit=\"\u001f\u001esaveTodo()\"><div class=\"form-group\"><label>Name:</label><input type=\"text\" class=\"form-control\" name=\"name\"></div><div class=\"form-group\"><button type=\"submit\" class=\"btn btn-default\">提交</button></div></form></div>",
     render: function() {
         this.setHTML(this.id, this.tmpl)
+    },
+
+    /**
+     * 事件监听器方括号内注明方法所监听的事件, 事件监听器的this为view
+     * @param e {Event} 所使用框架的event事件, 如果是jquery, 那就是jquery事件, kissy类似
+     **/
+    'saveTodo<submit>': function (e) {
+        e.preventDefault()
+
+        // 通过id查找模块容器DOM节点
+        var $main = $('#' + this.id)
+        var $name = $main.find('[name=name]')
+
+        $.ajax({
+            url: '/api/todo/add.json',
+            data: {
+                name: $name.val()
+            }
+        }).then(function (resp) {
+            if (resp.info.ok) {
+                alert('保存成功')
+                Magix.Router.to('/todo/list')
+            } else {
+                alert(resp.info.message)
+            }
+        })
     }
 })
 
